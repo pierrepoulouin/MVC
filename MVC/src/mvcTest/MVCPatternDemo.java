b388ca830d01791d02512650cff6d04b7a56b192
pierrepoulouin <pierrepoulouin@gmail.com>
2018-08-30 15:12:00 +0200

    First commit : debut du projet

---
+++ MVC/src/mvcTest/MVCPatternDemo.java
@@ -0,0 +1,30 @@
+package mvcTest;
+
+public class MVCPatternDemo {
+
+	public static void main(String[] args) {
+		//fetch student record based on his roll no from the database
+	      Student model  = retriveStudentFromDatabase();
+
+	      //Create a view : to write student details on console
+	      StudentView view = new StudentView();
+
+	      StudentController controller = new StudentController(model, view);
+
+	      controller.updateView();
+
+	      //update model data
+	      controller.setStudentName("TISTIS");
+
+	      controller.updateView();
+	   }
+
+	   private static Student retriveStudentFromDatabase(){
+	      Student student = new Student();
+	      student.setName("LeFadi");
+	      student.setRollNo("10");
+	      return student;
+
+	}
+
+}
